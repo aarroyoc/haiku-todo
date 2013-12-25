8c0b0c8248e5873bb6e61026bb6fb049bc905e60
Artur Jamro <artur.jamro@gmail.com>
2013-12-25 20:30:07 +0000

    [engine] Implemented Task class

--- engine/Task.cpp
+++ engine/Task.cpp
@@ -1,1 +1,467 @@
 #include "Task.h"
+#include "TaskList.h"
+
+#include <cassert>
+#include <sstream>
+
+#include <Autolock.h>
+
+
+namespace engine {
+	
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Static members
+//
+//////////////////////////////////////////////////////////////////////////////
+
+std::map<std::string, Task*> Task::sExistingTasks;
+BLocker Task::sExistingTasksMutex("Task::sExistingTasks mutex");
+
+
+void
+Task::_Register(Task& task)
+{
+	BAutolock guard(sExistingTasksMutex);
+	assert(sExistingTasks.find(task.GetId()) == sExistingTasks.end());
+		// One task can be registered only once
+	sExistingTasks[task.GetId()] = &task;
+}
+
+
+void
+Task::_Unregister(Task& task)
+{
+	BAutolock guard(sExistingTasksMutex);
+	assert(sExistingTasks.find(task.GetId()) != sExistingTasks.end());
+		// Task must be registered to unregister
+	sExistingTasks.erase(task.GetId());
+}
+
+
+Task*
+Task::GetById(std::string Id)
+{
+	static bool inited = false;
+	if(!inited) {
+		sExistingTasks[""] = nullptr;
+		inited = true;	
+	}
+	
+	BAutolock guard(sExistingTasksMutex);
+	assert(sExistingTasks.find(Id) != sExistingTasks.end());
+		// consider this assertion
+	return sExistingTasks.find(Id)->second;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Non-static members
+//	- constructors
+//  - destructor
+//
+//////////////////////////////////////////////////////////////////////////////
+
+std::string _GetNextId()
+{
+	static uint64 counter = 0;
+	counter++;
+	std::ostringstream str;
+	str << "!@#" << counter; // std::to_string not provided...
+	return str.str();
+}
+
+
+Task::Task(TaskList& owner, std::string title, std::string notes)
+	:
+	Task(owner, title, notes, 0)
+{
+}
+
+
+Task::Task(TaskList& owner, std::string title, std::string notes,
+	time_t dueDate)
+	:
+	fTitle(title),
+	fNotes(notes),
+	fDueDate(dueDate),
+	fId(_GetNextId()),
+	fCompleted(false),
+	fDeleted(false),
+	// skipping parent, children and siblings,
+	// default constructor is quite fine
+	fOwner(owner),
+	fLastUpdate(0),
+	fLastLocalChange(0),
+	fLastUpdateState(),
+	fMutex(("Task mutex, id: " + fId).c_str()),
+	fIsCopyObject(false)
+{
+	_Register(*this);
+}
+
+
+Task::~Task()
+{
+	if(fIsCopyObject)
+		return;
+	
+	_Unregister(*this);
+	_DeleteNode();
+}
+
+
+Task::Task(const Task& pattern)
+	:
+	fTitle(pattern.fTitle),
+	fNotes(pattern.fNotes),
+	fDueDate(pattern.fDueDate),
+	fId(pattern.fId),
+	fCompleted(pattern.fCompleted),
+	fDeleted(pattern.fDeleted),
+	fParentId(pattern.fParentId),
+	fFirstChildId(pattern.fFirstChildId),
+	fNextSiblingId(pattern.fNextSiblingId),
+	fPreviousSiblingId(pattern.fPreviousSiblingId),
+	fOwner(pattern.fOwner),
+	
+	fLastUpdate(0),
+	fLastLocalChange(0),
+	fLastUpdateState(),
+	fMutex(("[COPY] Task mutex, id" + fId).c_str()),
+	fIsCopyObject(true)
+{
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Non-static members
+//	- public getters
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+std::string
+Task::GetTitle() const
+{
+	BAutolock guard(fMutex);
+	return fTitle;
+}
+
+
+std::string
+Task::GetNotes() const
+{
+	BAutolock guard(fMutex);
+	return fNotes;
+}
+
+
+time_t
+Task::GetDueDate() const
+{
+	BAutolock guard(fMutex);
+	return fDueDate;
+}
+
+
+bool
+Task::IsCompleted() const
+{
+	BAutolock guard(fMutex);
+	return fCompleted;
+}
+
+
+bool
+Task::IsDeleted() const
+{
+	BAutolock guard(fMutex);
+	return fDeleted;
+}
+
+
+std::string
+Task::GetId() const
+{
+	BAutolock guard(fMutex);
+	return fId;
+}
+
+
+Task*
+Task::GetParent() const
+{
+	BAutolock guard(fMutex);
+	return GetById(fParentId);
+}
+
+
+Task*
+Task::GetFirstChild() const
+{
+	BAutolock guard(fMutex);
+	return GetById(fFirstChildId);
+}
+
+
+Task*
+Task::GetPreviousSibling() const
+{
+	BAutolock guard(fMutex);
+	return GetById(fPreviousSiblingId);
+}
+
+
+Task*
+Task::GetNextSibling() const
+{
+	BAutolock guard(fMutex);
+	return GetById(fNextSiblingId);
+}
+
+
+TaskList&
+Task::GetOwner() const
+{
+	// current object does *NOT* change owner, so we don't need thread-safety
+	return fOwner;	
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Non-static members
+//	- public setters
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+void
+Task::SetTitle(std::string title)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	fTitle = title;	
+}
+
+
+void
+Task::SetNotes(std::string notes)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	fNotes = notes;	
+}
+
+
+void
+Task::SetDueDate(time_t dueDate)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	fDueDate = dueDate;	
+}
+
+
+void
+Task::Complete(bool state, bool affectChildren, bool affectParent)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	fCompleted = state;
+	
+	if (affectChildren == true) {
+		// Mark all children recursively as done.
+		Task* child = GetById(fFirstChildId);
+		for ( ; child != nullptr; child = child->GetNextSibling())
+				child->Complete(state, true, false);
+	}
+	
+	if (state == false && affectParent == true) {
+		Task* parent = GetById(fParentId);
+		if (parent)
+			parent->Complete(false, false, false);
+	}
+}
+
+
+void
+Task::Delete()
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	fDeleted = true;
+}
+
+
+void
+Task::SetParent(Task* parent)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	_DeleteNode();
+	_InsertNode(nullptr, parent);	
+}
+
+
+void
+Task::SetPreviousSibling(Task* sibling)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	_DeleteNode();
+	_InsertNode(sibling, sibling ? sibling->GetParent() : GetById(fParentId));
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Non-static members
+//	- private methods
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+// Reserved for synchronizer (when local id is replaced by this from server)
+void
+Task::_ChangeId(std::string id)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	
+	// Re-register object
+	_Unregister(*this);
+		// ^ Consider it. Do let store old id?
+	fId = id;
+	_Register(*this);
+	
+	// Update ID in relatives
+	Task* parent = GetById(fParentId);
+	if (parent)
+		parent->_SetVarFirstChild(this, false);
+	Task* previousSibling = GetById(fPreviousSiblingId);
+	if (previousSibling)
+		previousSibling->_SetVarNextSibling(this, false);
+	Task* nextSibling = GetById(fNextSiblingId);
+	if (nextSibling)
+		nextSibling->_SetVarPreviousSibling(this, false);
+}
+
+
+// Mutex should be locked while calling this function, so we are thread-safe
+// and we cannot lock mutex (use thread-safe methods), cause we'll get locked
+// forever.
+void
+Task::_DeleteNode()
+{
+	Task* previousSibling = GetPreviousSibling();
+	Task* nextSibling = GetNextSibling();
+	
+	// Thread-problem: someone is iterating through siblings and: we lock
+	// mutex, neighbor returns pointer to us, we modify this neighbor,
+	// but iterating stops on us or - worse - is going from our new position!
+	// How to fix it? Mutex on siblings/parent-child connections?
+	if (nextSibling)
+		nextSibling->_SetVarPreviousSibling(previousSibling);
+	if (previousSibling)
+		previousSibling->_SetVarNextSibling(nextSibling);
+	else { // We are first child, so we should update parent's information
+		Task* parent = GetParent();
+		if (parent)
+			parent->_SetVarFirstChild(nextSibling);
+	}
+	fPreviousSiblingId = fNextSiblingId = fParentId = "";
+}
+
+
+// Mutex should be locked while calling this function, so we are thread-safe
+// and we cannot lock mutex (use thread-safe methods), cause we'll get locked
+// forever.
+void
+Task::_InsertNode(Task* previousSibling, Task* parent)
+{
+	fParentId = parent ? parent->GetId() : "";
+	Task* nextSibling = nullptr;
+	if (previousSibling) {
+		assert(previousSibling->GetParent() == parent);
+		
+		// We must lock previousSibling here, because we get it's next sibling
+		// and then set it - we cannot let someone read that data in middle
+		// time, cause it'll be inconsistent.
+		BAutolock(previousSibling->fMutex);
+		
+		fPreviousSiblingId = previousSibling->fId;
+		nextSibling = GetById(previousSibling->fNextSiblingId);
+		fNextSiblingId = nextSibling ? nextSibling->GetId() : "";
+			
+		previousSibling->fNextSiblingId = fId;
+	}
+	else {
+		if (parent) {
+			Task* nextSibling = parent->GetFirstChild();
+			parent->_SetVarFirstChild(this, false);
+			
+			fNextSiblingId = nextSibling ? nextSibling->GetId() : "";
+		}
+		else { // We are new root - so we have to find last one to replace it
+			if(!fOwner.GetTaskList().empty) {
+				Task* root = &*fOwner.GetTaskList().front();
+				while(root->GetParent() != nullptr)
+					root = root->GetParent();
+				while(root->GetPreviousSibling() != nullptr)
+					root = root->GetPreviousSibling();
+				
+				nextSibling = root;
+				fNextSiblingId = nextSibling->GetId();
+			}
+			else { // There's no root (empty TaskList)
+				nextSibling = nullptr;
+			}
+		}
+	}
+	
+	if (nextSibling)
+		nextSibling->_SetVarPreviousSibling(this, false);
+}
+
+
+void
+Task::_SetVarNextSibling(Task* sibling, bool targetSafeLock)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	if (sibling == nullptr)
+		fNextSiblingId = "";
+	else
+		fNextSiblingId = targetSafeLock ? sibling->GetId() : sibling->fId;
+}
+
+
+void
+Task::_SetVarPreviousSibling(Task* sibling, bool targetSafeLock)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	if (sibling == nullptr)
+		fPreviousSiblingId = "";
+	else
+		fPreviousSiblingId = targetSafeLock ? sibling->GetId() : sibling->fId;
+}
+
+
+void
+Task::_SetVarFirstChild(Task* child, bool targetSafeLock)
+{
+	assert(fIsCopyObject == false);
+	BAutolock guard(fMutex);
+	if (child == nullptr)
+		fFirstChildId = "";
+	else
+		fFirstChildId = targetSafeLock ? child->GetId() : child->fId;
+}
+
+	
+} // namespace engine
--- engine/Task.h
+++ engine/Task.h
@@ -7,8 +7,10 @@
 #include <memory>
 #include <string>
 
-#include <BLocker.h>
+#include <Locker.h>
 
+
+namespace engine {
 
 class TaskList;
 
@@ -17,25 +19,28 @@ class Task {
 		Task(TaskList& owner, std::string title = "", std::string notes = "");
 		Task(TaskList& owner, std::string title, std::string notes,
 			time_t dueDate);
+		~Task();
+			// Check desctructor after implementing SetNextSibling, etc!
 		
 		void						SetTitle(std::string title);
 		std::string					GetTitle() const;
 		void						SetNotes(std::string notes);
 		std::string					GetNotes() const;
 		void						SetDueDate(time_t date);
-		time						GetDueDate() const;
-		void						Complete(bool state, bool affectOthers
-										= false);
+		time_t						GetDueDate() const;
+		void						Complete(bool state,
+										bool affectChildren = true,
+										bool affectParent = true);
 		bool						IsCompleted() const;
 		void						Delete();
 		bool						IsDeleted() const;
 		std::string					GetId() const;
 		
-		void						SetParent(const Task* parent);
+		void						SetParent(Task* parent);
 										// nullptr means no parent (top tasks)
 		Task*						GetParent() const;
 		Task*						GetFirstChild() const;
-		void						SetPreviousSibling(const Task* sibling);
+		void						SetPreviousSibling(Task* sibling);
 										// nullptr means that this task is
 										// first in parent's child list
 		Task*						GetPreviousSibling() const;
@@ -45,12 +50,32 @@ class Task {
 		
 		
 	public:
-		static Task*				GetById(std::string Id);	
+		static Task*				GetById(std::string Id);
+			// Returns pointer instead reference, cause empty Id represents
+			// null pointer (nullptr).
 	
-	private:
-									Task();					
-										// creates copy object (other behavior)
-		std::unique_ptr<Task>		_CreateCopy() const;
+	private:	
+		// These functions create copy object (other behavior). Consider making
+		// these public - should object be non-copyable? It has unique id
+									Task(const Task& pattern);
+		Task&						operator=(const Task& pattern) = delete;
+		
+		// _ChangeId() is thread-safe
+		void						_ChangeId(std::string id);
+		// Two methods above are *NOT* thread-safe (it's desired)
+		void						_DeleteNode();
+		void						_InsertNode(Task* previousSibling,
+										Task* parent);
+		
+		// Some function for reporting changes!
+		
+		// Functions provided only for thread-safety
+		void						_SetVarNextSibling(Task* sibling,
+										bool targetSafeLock = true);
+		void						_SetVarPreviousSibling(Task* sibling,
+										bool targetSafeLock = true);
+		void						_SetVarFirstChild(Task* child,
+										bool targetSafeLock = true);
 		
 	private:
 		std::string					fTitle;
@@ -66,20 +91,26 @@ class Task {
 		std::string					fPreviousSiblingId;
 		TaskList&					fOwner;
 		
+		// Variables for synchronizer (not need thread safety)
 		time_t						fLastUpdate;
 			// if equals zero, then task is not created in online base,
 			// so after this creating, id will have new value
 		time_t						fLastLocalChange;
 		std::unique_ptr<Task>		fLastUpdateState;
 			// uses for diff update (better synchronization)
-		BLocker						fMutex;		
+		
+		mutable BLocker				fMutex;
+		const bool					fIsCopyObject;
 		
 	private:
-		static Task&				_Register(Task& task, std::string Id);
-		static Task&				_Unregister(std::string Id);
+		static void					_Register(Task& task);
+		static void					_Unregister(Task& task);
 		static std::map<std::string, Task*>
 									sExistingTasks;
+		static BLocker				sExistingTasksMutex;
 	
 };
+
+} // namespace engine
 
 #endif
