14c807c34c337538ebf7da880c72c01be91cb31e
Artur Jamro <artur.jamro@gmail.com>
2013-12-26 15:39:20 +0000

    [engine] Added TaskListManager header file

---
+++ engine/TaskListManager.cpp
@@ -0,0 +1,1 @@
+#include "TaskListManager.h"
---
+++ engine/TaskListManager.h
@@ -0,0 +1,65 @@
+#ifndef TASKLISTMANAGER_H
+#define TASKLISTMANAGER_H
+
+
+#include "PluginLoader.h"
+#include "TaskList.h"
+
+
+namespace plugin {
+
+class TaskSynchronizer;
+class TaskSerializer;
+	
+}
+
+namespace engine {
+	
+class TaskListManager {	
+	public:
+		typedef std::list<std::unique_ptr<TaskLists>> TaskListContainer;
+		
+		enum class InitType {
+			PluginFilename,
+			ContentFilename,
+			SerializedData
+		};
+		
+		
+	public:
+		// Constructor - class is able to hold only one plugin during lifetime,
+		//   so you must decide what you will be using before creating object.
+		//   Changing plugin during lifetime does *NOT* make sense, because it
+		//   can cause synchronization problems - how do you want synchronized
+		//   two online lists? If you want to support second list, simple
+		//   create new object.
+									TaskListManager(InitType type,
+										std::string data);
+		
+		const TaskListContainer&	GetLists() const;
+		void						AddList(std::string title ="Unnamed list");
+		
+		void						ClearDeletedLists();
+		
+		std::string					Serialize() const;
+		void						SaveToFile(std::string filename) const;
+			// File/Serialize format: first line contain plugin's filename,
+			// other data are serialized tasks.
+		
+	private:
+		void						_Parse(std::string content);
+		void						_ReadFromFile(std::string filename);
+		
+		TaskSynchronizer&			_GetSynchronizer() const;
+			// Should it be public? SynchronizeAll() outside system for a wish
+		TaskSerializer&				_GetSerializer() const;	
+		
+	private:
+		TaskListContainer			fTaskLists;
+		PluginLoader				fPluginLoader;
+};
+
+} // namespace engine
+
+
+#endif //TASKLISTMANAGER_H
