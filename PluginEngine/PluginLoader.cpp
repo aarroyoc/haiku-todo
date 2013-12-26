c833d4d2d84c63daded95fb28b881d9858aea7f6
Artur Jamro <artur.jamro@gmail.com>
2013-12-26 21:04:59 +0000

    [PluginEngine] Implemented PluginLoader class

--- PluginEngine/PluginLoader.cpp
+++ PluginEngine/PluginLoader.cpp
@@ -1,7 +1,161 @@
 #include "PluginLoader.h"
+#include "SynchronizationPlugin.h"
+
+#include <cassert>
+#include <dlfcn.h>
+#include <stdexcept>
 
 
 namespace PluginEngine {
+
+
+const char* const PluginLoader::PluginFactorySymbol
+									= "CreateSynchronizationPlugin";
+const char* const PluginLoader::PluginDestructorSymbol
+									= "DestroySynchronizationPlugin";
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Constructor & destructor
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+PluginLoader::PluginLoader(std::string filename)
+	:
+	fLibraryHandle(nullptr),
+	fPlugin(nullptr)
+{
+	Load(filename);
+}
+
+
+PluginLoader::~PluginLoader()
+{
+	if (IsPluginLoaded())
+		Unload();	
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Public methods - getters
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+bool
+PluginLoader::IsPluginLoaded() const
+{
+	return fPlugin != nullptr;	
+}
+
+
+SynchronizationPlugin&
+PluginLoader::GetPlugin() const
+{
+	assert(IsPluginLoaded());
+	return *fPlugin;	
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Public methods - setters
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+void
+PluginLoader::Load(std::string filename)
+{
+	assert(fPlugin == nullptr);
+	
+	if (filename == "") {
+		// Get default plugin (no synchronization)
+		fPlugin = new SynchronizationPlugin();
+		return;
+	}
+	
+	fLibraryHandle = dlopen(filename.c_str(), RTLD_LAZY);
+	if (fLibraryHandle == nullptr) {
+		throw std::runtime_error("PluginLoader::Load() - Cannot load library "
+			"\"" + filename + "\" due to: " + dlerror());
+	}
+	
+	try {
+		PluginFactoryPtr factory = reinterpret_cast<PluginFactoryPtr>(
+			_GetSymbol(PluginFactorySymbol));
+		_GetSymbol(PluginDestructorSymbol);
+			// Just ensure to get error here instead during unloading
+	
+		fPlugin = factory();
+	}
+	catch (std::runtime_error) {
+		dlclose(fLibraryHandle);
+		fLibraryHandle = nullptr;
+		
+		throw;	
+	}
+	
+	fPluginFilename = filename;
+}
+
+
+void
+PluginLoader::Unload()
+{
+	assert(fPlugin != nullptr);
+	
+	if (fLibraryHandle == nullptr) {
+		// using default plugin (no synchronization)
+		delete fPlugin;
+		fPlugin = nullptr;
+		return;	
+	}
+	
+	PluginDestructorPtr destructor = reinterpret_cast<PluginDestructorPtr>(
+		_GetSymbol(PluginDestructorSymbol));
+	destructor(fPlugin);
+	dlclose(fLibraryHandle);
+	fLibraryHandle = fPlugin = nullptr;
+	fPluginFilename = "";
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Private methods
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+void*
+PluginLoader::_GetSymbol(std::string symbol) const
+{
+	assert(fLibraryHandle != nullptr);
+	
+	while (dlerror());
+		// Clear any existing error
+	
+	void* exportedSymbol = dlsym(fLibraryHandle, symbol.c_str());
+	
+	if (exportedSymbol == nullptr) {		
+		std::string message = "PluginLoader::_GetSymbol() - Symbol \"" + symbol
+			+ "\"not found in library \"" + fPluginFilename + "\"";
+		const char* error = dlerror();
+		if (error != nullptr)
+			message += std::string(" due to: ") + error;
+		
+		throw std::runtime_error(message);
+	}
+	
+	while (dlerror());
+		// Clear any existing error
+	
+	return exportedSymbol;
+}
 
 
 } // namespace PluginEngine
--- PluginEngine/PluginLoader.h
+++ PluginEngine/PluginLoader.h
@@ -11,7 +11,7 @@ class SynchronizationPlugin;
 
 class PluginLoader {
 	public:
-									PluginLoader() = default;
+									PluginLoader(); // = default;
 									PluginLoader(std::string filename);
 									~PluginLoader();
 	
@@ -22,9 +22,27 @@ class PluginLoader {
 		
 		
 	private:
+		void*						_GetSymbol(std::string symbol) const;
+										// Helper function for Load() and
+										// Unload(). CAUTION! If symbol is not
+										// found exception will be thrown.
+		
+		
+	private:
 		void*						fLibraryHandle;
 		SynchronizationPlugin*		fPlugin;
+		std::string					fPluginFilename;
+			// variable for std::runtime_error in _GetSymbol
+		
+		
+	private:
+		static const char* const	PluginFactorySymbol;
+									//	= "CreateSynchronizationPlugin";
+		static const char* const	PluginDestructorSymbol;
+									//	= "DestroySynchronizationPlugin";
 	
+		typedef SynchronizationPlugin* (*PluginFactoryPtr)();
+		typedef void (*PluginDestructorPtr)(SynchronizationPlugin*);
 };
 
 } // namespace PluginEngine
