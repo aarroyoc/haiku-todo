40b0b3a6d96d1c7fb64c74eb4720e5180df36b26
Artur Jamro <artur.jamro@gmail.com>
2013-12-28 20:50:53 +0000

    [PluginEngine] Created and implemented TaskSynchronizer class

---
+++ PluginEngine/TaskSynchronizer.cpp
@@ -0,0 +1,265 @@
+#include "TaskListManager.h"
+#include "TaskSynchronizer.h"
+
+#include "Internet.h"
+
+#include <iostream>
+#include <stdexcept>
+
+#include <Autolock.h>
+#include <kernel/OS.h>
+
+
+namespace PluginEngine {
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Constructor & Destructor
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+TaskSynchronizer::TaskSynchronizer(AppEngine::TaskListManager& manager,
+	bool synchronizationEnabled, int32 synchronizationTimestampSeconds)
+	:
+	fManager(manager),
+	fSynchronizationEnabled(synchronizationEnabled),
+	fSynchronizationTimestamp(synchronizationTimestampSeconds),
+	fSynchronizationStopwatch("TaskSynchronization Stopwatch", true)
+{
+	fWorkerThreadId = spawn_thread(_SynchronizationWorker,
+		"Task list synchronization worker",	B_NORMAL_PRIORITY, this);
+	if (fWorkerThreadId < B_OK)
+		throw std::runtime_error("TaskSynchronization::(constructor) - Cannot "
+			"create worker thread!");
+	resume_thread(fWorkerThreadId);
+}
+
+
+TaskSynchronizer::~TaskSynchronizer()
+{
+	fDestructorCalled = true;
+	status_t exitValue;
+	wait_for_thread(fWorkerThreadId, &exitValue);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Public methods - getters
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+bool
+TaskSynchronizer::IsSynchronizationEnabled() const
+{
+	BAutolock guard(fDataMutex);
+	return fSynchronizationEnabled;
+}
+
+
+int32
+TaskSynchronizer::GetSynchronizationTimestamp() const
+{
+	BAutolock guard(fDataMutex);
+	return fSynchronizationTimestamp;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Public methods - setters
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+void
+TaskSynchronizer::EnableSynchronization(bool enabled)
+{
+	BAutolock guard(fDataMutex);
+	fSynchronizationEnabled = enabled;	
+}
+
+
+void
+TaskSynchronizer::SetSynchronizationTimestamp(int32 seconds)
+{
+	BAutolock guard(fDataMutex);
+	fSynchronizationTimestamp = seconds;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Public methods - synchronize requests
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+void
+TaskSynchronizer::SynchronizeAll()
+{
+	BAutolock guard(fQueueMutex);
+
+	Request request = { Request::RequestType::SynchronizeAll, { nullptr } };
+	fAwaitingRequests.push_front(request);
+		// priority request!
+}
+
+
+void
+TaskSynchronizer::SynchronizeList(AppEngine::TaskList& list)
+{
+	BAutolock guard(fQueueMutex);
+
+	Request request = { Request::RequestType::SynchronizeList,
+		{ (AppEngine::Task*)&list } };
+		// casting to mute warning about missing braces
+	fAwaitingRequests.push_back(request);
+}
+
+
+void
+TaskSynchronizer::SynchronizeTask(AppEngine::Task& task)
+{
+	BAutolock guard(fQueueMutex);
+
+	Request request = { Request::RequestType::SynchronizeTask, { &task } };
+	fAwaitingRequests.push_back(request);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Private method - synchronization worker (other thread)
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+int32
+TaskSynchronizer::_SynchronizationWorker(void* synchronizerPtr)
+{
+	TaskSynchronizer* synchronizer =
+		reinterpret_cast<TaskSynchronizer*>(synchronizerPtr);
+	
+	bool connectedToInternet = Internet::CheckConnection();
+	BAutolock queueGuard(synchronizer->fQueueMutex);
+	queueGuard.Unlock();
+	
+	while (synchronizer->fDestructorCalled == false) {
+		
+		bigtime_t sleepTimestamp = 1e6; // 1e6 us = 1 sec
+		snooze(sleepTimestamp);
+		
+		if (synchronizer->IsSynchronizationEnabled() == false)
+			continue;
+		
+		
+		while (connectedToInternet == false) {
+			connectedToInternet = Internet::CheckConnection();
+			
+			bigtime_t sleepTimestamp = 1e7; // 1e7 us = 10 sec
+			snooze(sleepTimestamp);
+		}
+		
+		queueGuard.Lock();
+		if (synchronizer->fSynchronizationStopwatch.ElapsedTime() / 1e6
+			> synchronizer->GetSynchronizationTimestamp() ||
+			(synchronizer->fAwaitingRequests.empty() == false &&
+			synchronizer->fAwaitingRequests.front().type ==
+			Request::RequestType::SynchronizeAll))
+		{
+			queueGuard.Unlock();
+			if (synchronizer->_OnSynchronizeAll() == true) {
+				queueGuard.Lock();
+				synchronizer->fAwaitingRequests.clear();
+				queueGuard.Unlock();
+				synchronizer->fSynchronizationStopwatch.Reset();
+			}
+			else {
+				connectedToInternet = Internet::CheckConnection();
+				if (connectedToInternet == false) 
+					continue;
+				
+				std::cerr << "TaskSynchronizer::_SynchronizationWorker: "
+					"_OnSynchronizeAll() failed when Internet connection "
+					"was available!\n";
+				// Callback?
+			}
+		}
+		
+		queueGuard.Lock();
+		while (synchronizer->fAwaitingRequests.empty() == false) {
+			Request request = synchronizer->fAwaitingRequests.front();
+			synchronizer->fAwaitingRequests.pop_front();
+			queueGuard.Unlock();
+			
+			bool result = true;
+			switch(request.type) {
+				case Request::RequestType::SynchronizeList:
+					result = synchronizer->_OnSynchronizeList(*request.list);
+					break;
+					
+				case Request::RequestType::SynchronizeTask:
+					result = synchronizer->_OnSynchronizeTask(*request.task);
+					break;
+					
+				default:
+					break;
+			}
+			
+			if (result == true)
+				continue;
+			
+			
+			connectedToInternet = Internet::CheckConnection();
+			if (connectedToInternet == false)
+				break;
+				
+			std::cerr << "TaskSynchronizer::_SynchronizationWorker: "
+				"_OnSynchronizeList/Task() failed when Internet connection "
+				"was available!\n";
+			// Callback?
+			// Don't panic about data lost - realtime synchronization will
+			// update all.
+		}
+	}
+	
+	return 0;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//	Protected methods - default synchronization (stub)
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+bool
+TaskSynchronizer::_OnSynchronizeAll()
+{
+	return true;
+}
+
+
+bool
+TaskSynchronizer::_OnSynchronizeList(AppEngine::TaskList& list)
+{
+	BString title = list.GetTitle();
+		// just eliminate "unused variable" warning
+	return true;
+}
+
+
+bool
+TaskSynchronizer::_OnSynchronizeTask(AppEngine::Task& task)
+{
+	BString title = task.GetTitle();
+		// just eliminate "unused variable" warning
+	return true;
+}
+
+
+} // namespace PluginEngine
---
+++ PluginEngine/TaskSynchronizer.h
@@ -0,0 +1,98 @@
+#ifndef TASKSERIALIZER_H
+#define TASKSERIALIZER_H
+
+
+#include <list>
+
+#include <Locker.h>
+#include <StopWatch.h>
+#include <SupportDefs.h>
+
+
+namespace AppEngine {
+
+class TaskListManager;
+class TaskList;
+class Task;
+	
+} // namespace AppEngine
+
+
+namespace PluginEngine {
+
+class TaskSynchronizer {
+	public:
+							TaskSynchronizer(
+								AppEngine::TaskListManager& manager,
+								bool synchronizationEnabled = true,
+								int32 synchronizationTimestampSeconds = 60);
+		virtual				~TaskSynchronizer();
+							
+			// Object is non-copyable (due to inter alia thread worker)
+							TaskSynchronizer(const TaskSynchronizer&) = delete;
+		TaskSynchronizer&	operator=(const TaskSynchronizer&) = delete;
+	
+		void				EnableSynchronization(bool enabled);
+		bool				IsSynchronizationEnabled() const;
+		void				SetSynchronizationTimestamp(int32 seconds);
+		int32				GetSynchronizationTimestamp() const;
+		
+		void				SynchronizeAll();
+		void				SynchronizeList(AppEngine::TaskList& list);
+		void				SynchronizeTask(AppEngine::Task& task);
+		
+		// Add: callbacks with result (failed, updated: changed or not)
+		
+	
+	private:
+		static int32		_SynchronizationWorker(void* synchronizer);
+	
+	
+	protected:
+		// Methods to define in plugin for synchronization.
+		// Default behaviour - do nothing.
+		virtual bool		_OnSynchronizeAll();
+		virtual bool		_OnSynchronizeList(AppEngine::TaskList& list);
+		virtual bool		_OnSynchronizeTask(AppEngine::Task& task);
+	
+		
+	private:
+		struct Request {
+			enum class RequestType {
+				SynchronizeTask,
+				SynchronizeList,
+				SynchronizeAll
+			} type;
+			union {
+				AppEngine::Task*		task;
+				AppEngine::TaskList*	list;
+			};
+		};
+		
+		
+	protected:
+		AppEngine::TaskListManager&
+							fManager;
+	
+	
+	private:
+		bool				fSynchronizationEnabled;
+		int32				fSynchronizationTimestamp;
+		BStopWatch			fSynchronizationStopwatch;
+		thread_id			fWorkerThreadId;
+		
+		std::list<Request>	fAwaitingRequests;
+		
+		mutable BLocker		fDataMutex;
+		mutable BLocker		fQueueMutex;
+			// More mutexes for non-blocking main thread (which can read and
+			// set informations about synchronization) and worker thread 
+			// (which manage queues).
+		
+		bool				fDestructorCalled;
+			// information for thread worker
+};
+
+} // namespace PluginEngine
+
+#endif // TASKSERIALIZER_H
